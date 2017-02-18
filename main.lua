0c128954da52470e37ca56aa90a7a5f2df0f476d
enra64 <sujomear@arcor.de>
2017-02-18 14:18:18 +0100

    added explosions that could use some polishing

--- main.lua
+++ main.lua
@@ -9,7 +9,11 @@ bullet_speed = 1000
 enemy_speed = 200
 speed = 400
 
+explosions = {}
+
 score = 0
+
+maximum_explosion_age = .2
 
 shot_pressed = false
 
@@ -20,11 +24,11 @@ local function check_collides(a, b)
             a.height + a.y > b.y
 end
 
-local function remove_all_colliding(collection, colliding_object, collision_handling_function)
+local function remove_all_colliding(collection, colliding_object, collision_handler)
     local had_collision = false
     for index, object in ipairs(collection) do
         if check_collides(object, colliding_object) then
-            collision_handling_function(object)
+            collision_handler(colliding_object, object)
             table.remove(collection, index)
             had_collision = true
         end
@@ -45,7 +49,7 @@ end
 local function create_enemy()
     local new_enemy = {}
 
-    -- load texture
+    --- load texture
     new_enemy.texture = love.graphics.newImage("enemy_with_propulsion.png")
 
     --- store width and height
@@ -53,6 +57,9 @@ local function create_enemy()
     local height = new_enemy.texture:getHeight()
     new_enemy.width = width
     new_enemy.height = height
+
+    --- no scaling
+    new_enemy.scale = 1
 
     --- find free position
     repeat
@@ -68,11 +75,15 @@ local function shoot(x, y)
 
     new_bullet.texture = love.graphics.newImage("missile_with_propulsion.png")
 
+    --- store scaling factor
+    new_bullet.scale = .6
+
     --- store width and height
     local width = new_bullet.texture:getWidth()
     local height = new_bullet.texture:getHeight()
-    new_bullet.width = width
-    new_bullet.height = height
+    new_bullet.width = width * new_bullet.scale
+    new_bullet.height = height * new_bullet.scale
+
 
     --- init pos
     new_bullet.x = x
@@ -80,6 +91,46 @@ local function shoot(x, y)
 
     --- add new bullet to list
     table.insert(bullets, new_bullet)
+end
+
+local function create_explosion(x, y)
+    explosion = {}
+    explosion.corona_texture = love.graphics.newImage("explosion_blue_ring.png")
+    explosion.main_texture = love.graphics.newImage("explosion.png")
+
+    --- age determines the scaling of explosions
+    explosion.age = 0
+    explosion.x = x
+    explosion.y = y
+
+    --- add some variation using random rotation
+    explosion.corona_rotation = math.rad(math.random(360))
+    explosion.main_rotation = math.rad(math.random(360))
+
+    --- store explosion
+    table.insert(explosions, explosion)
+end
+
+local function handle_kill(_, killed_enemy)
+    create_enemy()
+    score = score + 10
+
+    --- make explody thing over enemy. explosions are drawn at their center point,
+    create_explosion(killed_enemy.x, killed_enemy.y)
+end
+
+local function update_bullets(dt)
+    for i, bullet in ipairs(bullets) do
+        bullet.x = bullet.x + (bullet_speed * dt)
+
+        if bullet.x > love.graphics.getWidth() then
+            table.remove(bullets, i)
+        end
+
+        if remove_all_colliding(enemies, bullet, handle_kill) then
+            table.remove(bullets, i)
+        end
+    end
 end
 
 local function update_enemies(dt)
@@ -119,21 +170,13 @@ local function update_player(dt)
     end
 end
 
-local function handle_kill()
-    create_enemy()
-    score = score + 10
-end
+local function update_explosions(dt)
+    for index, explosion in ipairs(explosions) do
+        explosion.age = explosion.age + dt
 
-local function update_bullets(dt)
-    for i, bullet in ipairs(bullets) do
-        bullet.x = bullet.x + (bullet_speed * dt)
-
-        if bullet.x > love.graphics.getWidth() then
-            table.remove(bullets, i)
-        end
-
-        if remove_all_colliding(enemies, bullet, handle_kill) then
-            table.remove(bullets, i)
+        -- remove old explosions
+        if explosion.age > maximum_explosion_age then
+            table.remove(explosions, index)
         end
     end
 end
@@ -142,19 +185,38 @@ function love.update(dt)
     update_bullets(dt)
     update_player(dt)
     update_enemies(dt)
+    update_explosions(dt)
 end
 
 local function draw_all_in(collection)
     for _, item in ipairs(collection) do
-        love.graphics.draw(item.texture, item.x, item.y, 0, 1, 1)
+        love.graphics.draw(item.texture, item.x, item.y, 0, item.scale)
+    end
+end
+
+local function draw_explosions()
+    for _, explosion in ipairs(explosions) do
+        love.graphics.setColor(255, 255, 255, 255 - (255 * explosion.age / maximum_explosion_age))
+        local corona_scaling_factor = explosion.age * 2 / maximum_explosion_age
+        local corona_offset_x = explosion.corona_texture:getWidth() / 2
+        local corona_offset_y = explosion.corona_texture:getHeight() / 2
+        love.graphics.draw(explosion.corona_texture, explosion.x, explosion.y, explosion.corona_rotation, corona_scaling_factor, corona_scaling_factor, corona_offset_x, corona_offset_y)
+
+        love.graphics.setColor(255, 255, 255)
+        local main_scaling_factor = explosion.age / maximum_explosion_age
+        local main_offset_x = explosion.main_texture:getWidth() / 2
+        local main_offset_y = explosion.main_texture:getHeight() / 2
+        love.graphics.draw(explosion.main_texture, explosion.x, explosion.y, explosion.main_rotation, main_scaling_factor, main_scaling_factor, main_offset_x, main_offset_y)
     end
 end
 
 function love.draw()
+    --- background
     love.graphics.draw(background, 0, 0, 0, love.graphics.getWidth() / background:getWidth(), love.graphics.getHeight() / background:getHeight())
 
     draw_all_in(enemies)
     draw_all_in(bullets)
+    draw_explosions()
     love.graphics.draw(player.texture, player.x, player.y, 0, 1, 1)
 end
 
