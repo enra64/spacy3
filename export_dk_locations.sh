316280547db3d5ec5ce210d043aaae7f859460cb
enra64 <sujomear@arcor.de>
2017-02-21 17:03:14 +0100

    moved raw files, added script for android deployment

---
+++ export_dk_locations.sh
@@ -0,0 +1,19 @@
+# export all the variables
+export ANDROID_SDK=/opt/sdk-android
+export ANDROID_NDK=/home/arne/Programs/ndk-android/
+export ANDROID_SWT=/usr/share/java
+export ANDROID_HOME=${ANDROID_SDK}
+export PATH=$PATH:$ANDROID_SDK/tools:$ANDROID_NDK
+
+# zip to game.love
+zip -qr -9 game.love . -x "xcf/*" -x ".idea/*" -x spacy3.iml -x "out/*" -x .gitignore -x "raw/*"
+
+# move the game.love to the love-android folder
+mkdir -p /home/arne/Programs/love-android-sdl2/assets
+mv game.love /home/arne/Programs/love-android-sdl2/assets/
+
+# move to my love-android folder
+cd /home/arne/Programs/love-android-sdl2/
+
+# DEPLOY
+#ant debug
