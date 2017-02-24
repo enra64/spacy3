2496b658f74c533c538a1ac218ffcd8b3c1bf507
enra64 <sujomear@arcor.de>
2017-02-24 13:25:37 +0100

    made buttons clickable

--- main.lua
+++ main.lua
@@ -10,12 +10,19 @@ gamestate = require "hump.gamestate"
 local game = ""
 local pause = require("pause")
 
+function on_pause_button_clicked(button_text)
+    print(button_text.." clicked")
+end
+
 function love.load()
     --- unshittify random numbers
     math.randomseed(os.time())
 
     --- set some window size
     love.window.setMode(1024, 768)
+
+    --- set pause menu callback
+    pause.on_button_clicked = on_pause_button_clicked
 
     gamestate.registerEvents()
     gamestate.switch(pause)
--- pause.lua
+++ pause.lua
@@ -8,31 +8,43 @@
 
 local pause = {}
 pause.horizontal_button_distance = 20
-
+pause.collisions = require("collisions")
 
 local function add_button(pause, text)
     table.insert(pause.button_texts, text)
+
+    --- re-calculate all button rectangles
+    pause.button_rectangles = {}
+    pause.button_rectangle_x_scale = pause.menu_width / pause.button_texture:getWidth()
+    pause.button_rectangle_y_scale = ((pause.menu_height - pause.horizontal_button_distance * #pause.button_texts) / pause.button_texture:getHeight()) / #pause.button_texts
+
+    for i = 1, #pause.button_texts do
+        local button_rect = {}
+        button_rect.width = pause.menu_width
+        button_rect.height = pause.button_rectangle_y_scale * pause.button_texture:getHeight()
+        button_rect.x = pause.menu_x
+        button_rect.y = pause.menu_y + ((i - 1) * (button_rect.height + pause.horizontal_button_distance)) + button_rect.height / 4
+        table.insert(pause.button_rectangles, button_rect)
+    end
 end
 
 function pause:update()
 end
 
 function pause:draw()
-    local x_scale = self.menu_width / self.button_texture:getWidth()
-    local y_scale = ((self.menu_height - self.horizontal_button_distance * #self.button_texts) / self.button_texture:getHeight()) / #self.button_texts
-    local button_height = y_scale * self.button_texture:getHeight()
 
-    for i = 1, #self.button_texts do
-        local y_pos = self.menu_y + ((i - 1) * (button_height + self.horizontal_button_distance))
-        love.graphics.draw(self.button_texture, self.menu_x, y_pos, 0, x_scale, y_scale)
+    for i, button_rect in ipairs(self.button_rectangles) do
+        love.graphics.draw(self.button_texture, button_rect.x, button_rect.y, 0, self.button_rectangle_x_scale, self.button_rectangle_y_scale)
+
         love.graphics.setColor(0, 0, 0)
-        love.graphics.printf(self.button_texts[i], self.menu_x + self.menu_width / 4, self.menu_y + ((i - 1) * (button_height + self.horizontal_button_distance)) + button_height / 4, self.menu_width / 2, "left", 0, 3)
+        love.graphics.printf(self.button_texts[i], self.menu_x + self.menu_width / 4, self.menu_y + ((i - 1) * (button_rect.height + self.horizontal_button_distance)) + button_rect.height / 4, self.menu_width / 2, "left", 0, 3)
         love.graphics.setColor(255, 255, 255)
     end
 end
 
 function pause:init()
     self.button_texts = {}
+    self.button_rectangles = {}
 
     --- store the target menu size and position
     self.menu_width = love.graphics.getWidth() / 2
@@ -49,6 +61,11 @@ function pause:init()
 end
 
 function pause:mousepressed(x, y, button)
+    for i, button_rect in ipairs(self.button_rectangles) do
+        if self.collisions.has_collision_point_rectangle(x, y, button_rect) then
+            self.on_button_clicked(self.button_texts[i])
+        end
+    end
 end
 
 return pause
