464786da82f15ec595c805c7f20beb13af41c757
enra64 <sujomear@arcor.de>
2017-03-29 20:37:00 +0200

    reduced difficulty in later game

--- difficulty_handler.lua
+++ difficulty_handler.lua
@@ -14,11 +14,11 @@ local desktop_value_dictionary = {
 
 local common_value_dictionary = {
     player_speed = 400,
-    enemy_simple_speed = {200, 250, 300, 350, 400},
+    enemy_simple_speed = {200, 250, 300, 325, 350},
     enemy_simple_score = {2, 4, 8, 16, 32},
-    level_threshold = {10, 45, 100, 200, 400},
+    level_threshold = {10, 45, 150, 300, 700},
     enemy_simple_count = {3, 4, 5, 5, 6},
-    asteroid_period = {0, 3, 2.8, 2.3, 1.7},
+    asteroid_period = {4, 3, 2.8, 2.6, 2.2},
     asteroid_speed = {100, 120, 150, 180, 220}
 }
 
