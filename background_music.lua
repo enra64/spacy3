2e5d04b565d4646af0bbc5b3d71d831286c70212
enra64 <sujomear@arcor.de>
2017-04-07 15:27:02 +0200

    beginning on background music work

---
+++ background_music.lua
@@ -0,0 +1,24 @@
+background_music = {}
+
+local tracks = {
+    store = {
+        love.sound.newSoundData("tracks/arrival.wav")
+    },
+    ingame = {
+        
+    },
+    killscreen = {
+        
+    },
+    main_menu = {
+        
+    }
+}
+
+background_music.init = function()
+    
+end
+
+background_music.play = function(category)
+    
+end
