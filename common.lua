867084fade323803bb66c808c868449be2cd0503
enra64 <sujomear@arcor.de>
2017-06-11 15:14:45 +0200

    fixed print_table -.-

--- common.lua
+++ common.lua
@@ -102,7 +102,7 @@ function print_table( t )
             if (type(t)=="table") then
                 for pos,val in pairs(t) do
                     if (type(val)=="table") then
-                        print(indent.."["..pos.."] => "..tostring(t).." {")
+                        print(indent.."["..pos.."] => "..tostring(val).." {")
                         sub_print_r(val,indent..string.rep(" ",string.len(pos)+8))
                         print(indent..string.rep(" ",string.len(pos)+6).."}")
                     elseif (type(val)=="string") then
